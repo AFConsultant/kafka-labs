0d4ceea6e5c7f05a712e6cf97246114c607ac38d
Alexandre Fruchaud <alexandre.fruchaud@gmail.com>
2025-06-06 12:14:37 +0000

    Producer sends messages to Kafka

--- ProducerApi/Program.cs
+++ ProducerApi/Program.cs
@@ -1,19 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
 
-builder.Services.AddOpenApi();
+var producerConfig = new ProducerConfig
+{
+    BootstrapServers = "kafka:9092"
+};
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<IProducer<Null, string>>(_ =>
+    new ProducerBuilder<Null, string>(producerConfig).Build());
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
 const string filePath = "/workspaces/kafka-labs/data/201306-citibike-tripdata_1_6K.csv";
+const string topicName = "bike_trips_raw_csv";
 
-app.MapPost("/trip", async () => {
+app.MapPost("/trip", ([FromServices] IProducer<Null, string> producer) => {
     
     if (!File.Exists(filePath))
     {
@@ -22,25 +27,28 @@ app.MapPost("/trip", async () => {
 
     _ = Task.Run(async () => {
         try
-        {
-            Console.WriteLine("--- Démarrage de la lecture du fichier de trajets ---");
-            
+        {            
             await foreach (var line in File.ReadLinesAsync(filePath))
             {
-                Console.WriteLine(line);
+                var message = new Message<Null, string> { Value = line };
+                try
+                {
+                    await producer.ProduceAsync(topicName, message);
+                    Console.WriteLine($"Ligne envoyée à Kafka : {line}");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Échec de l'envoi du message : {message.Value} | Raison : {e.Error.Reason}");
+                }
                 await Task.Delay(1000);
             }
-            
-            Console.WriteLine("--- Fin de la lecture du fichier ---");
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Une erreur est survenue lors de la lecture du fichier : {ex.Message}");
         }
     });
-    return Results.Accepted(value: "La lecture du fichier a démarré en arrière-plan.");
-})
-.WithName("PostTripData")
-.WithOpenApi();
+    return Results.Accepted();
+});
 
 app.Run();
