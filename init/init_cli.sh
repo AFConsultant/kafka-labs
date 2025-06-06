4afd5a35b84809dd06651fdc8cb5345e010956cf
Alexandre Fruchaud <alexandre.fruchaud@gmail.com>
2025-06-06 08:08:18 +0000

    Add init cli and network scripts

--- init/init_cli.sh
+++ init/init_cli.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo '127.0.0.1 kafka' | sudo tee -a /etc/hosts
-echo '127.0.0.1 zookeeper' | sudo tee -a /etc/hosts
-
 BASHRC="$HOME/.bashrc"
 MARKER="# Kafka CLI commands"
 
---
+++ init/init_network.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo '127.0.0.1 kafka' | sudo tee -a /etc/hosts
+echo '127.0.0.1 zookeeper' | sudo tee -a /etc/hosts
