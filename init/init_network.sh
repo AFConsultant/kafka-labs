6a291885ffcb16c0e9e46c1942635792ab8c817f
Alexandre Fruchaud <alexandre.fruchaud@gmail.com>
2025-06-06 15:04:50 +0000

    Add schema registry and ksqldb

--- init/init_network.sh
+++ init/init_network.sh
@@ -2,3 +2,5 @@
 
 echo '127.0.0.1 kafka' | sudo tee -a /etc/hosts
 echo '127.0.0.1 zookeeper' | sudo tee -a /etc/hosts
+echo '127.0.0.1 ksqldb' | sudo tee -a /etc/hosts
+echo '127.0.0.1 schema-registry' | sudo tee -a /etc/hosts
